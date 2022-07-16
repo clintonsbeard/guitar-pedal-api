03de13d7dba839c115ae11bedc9c396a094d6b34
Clinton Beardsley <clintonb@resourceinternational.com>
2022-07-15 20:21:44 -0400

    Testing first commit to Github via IntelliJ IDEA terminal/SSH key.

---
+++ src/test/java/com/clintonbeardsley/guitarpedalapi/GuitarPedalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clintonbeardsley.guitarpedalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuitarPedalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
